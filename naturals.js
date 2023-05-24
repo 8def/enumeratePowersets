970bd9c5ebe095b3292ea5cbcd29c5ca860105ea
Thomas Palmer <143a.def@gmail.com>
2023-05-24 12:58:19 -0700

    Create naturals.js

---
+++ naturals.js
@@ -0,0 +1,70 @@
+var limit = 20 // how many primes used to construct the powerset
+
+//biject P(p) and P(n)
+const ppBiject = arr => {
+	var output = []
+	for (let i=0; i<arr.length; i++) {
+  	output[i] = []
+  	for (let j=0; j<arr[i].length; j++) {
+    	output[i].push(primes.indexOf(arr[i][j])+1)
+    }
+  }
+  return output
+}
+
+// biject the squarefrees and the naturals
+const sBiject = arr => {
+	let j=0, bijection = []
+	for (let i=0; i<arr.length; i++) {
+  	if (arr[i] === undefined) { } else {
+      bijection[j] = arr[i]
+      j++
+    }
+  }
+  return bijection
+}
+
+// biject P(p) and the squarefrees
+const pBiject = arr => {
+	let product = 1, bijection = []
+	for (let i=0; i<arr.length; i++) {
+  	for (let j=0; j<arr[i].length; j++) {
+    	product = product*arr[i][j]
+    }
+    bijection[product] = arr[i]
+    product = 1
+  }
+  return bijection
+}
+// generate a powerset of a set of numbers
+const powerSet = nums => {
+  const result = [];
+  backtrack([], 0);
+  function backtrack(currSet, index) {
+    result.push(currSet);
+    
+    for (let i = index; i < nums.length; i++) {
+      backtrack([...currSet, nums[i]], i + 1);
+    }
+  }
+  return result;
+}
+// generate an array of primes between 1 and some limit
+const generatePrimes = limit => {
+	let primes = []
+	for (let i=2; i<=limit; i++) {
+  	if(isPrime(i))
+    	primes.push(i)
+  }
+  return primes
+}
+// determine if a number is prime
+const isPrime = num => {
+    for(let i = 2, s = Math.sqrt(num); i <= s; i++) {
+        if(num % i === 0) return false;
+    }
+    return num > 1;
+}
+
+var primes = generatePrimes(limit)
+console.log(ppBiject(sBiject(pBiject(powerSet(primes)))))
